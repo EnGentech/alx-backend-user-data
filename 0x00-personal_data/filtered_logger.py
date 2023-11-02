7890ac786259a75bbab758c4355446d949ab9d6c
EnGentech <engen.inyang@gmail.com>
2023-11-02 19:20:42 +0100

    another fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,18 +3,15 @@
 function called filter_datum will return the log message obfuscated
 """
 
-import logging
+from typing import List
 import re
 
 
-def filter_datum(fields: list, redaction:
+def filter_datum(fields: List[str], redaction:
                  str, message: str, seperator: str) -> str:
-    """
-    Basic loggin function
-    """
+    """Basic loggin function"""
     for specifics in fields:
-        pattern = rf'{specifics}=.+?{seperator}'
-        message = re.sub(pattern,
+        message = re.sub(rf'{specifics}=.+?{seperator}',
                          f"{specifics}={redaction}{seperator}", message)
     return message
 
