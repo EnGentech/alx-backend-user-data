9c66c565a699780c68bce3b209e089728c568360
EnGentech <engen.inyang@gmail.com>
2023-11-02 23:14:18 +0100

    refixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,8 +27,8 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields):
         """Initialization"""
-        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """format function definition"""
