6c3fc21ee57252d7477e53f965aec39cc6e92793
EnGentech <engen.inyang@gmail.com>
2023-11-02 22:51:21 +0100

    filtered log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ function called filter_datum will return the log message obfuscated
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -14,5 +15,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(rf"{specifics}=(.*?)\{separator}",
                          f'{specifics}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """Initialization"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format function definition"""
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
 
 # Coded by EnGentech
