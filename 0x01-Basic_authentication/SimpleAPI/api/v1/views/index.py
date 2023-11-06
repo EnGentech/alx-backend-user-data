c1eb42687941d364d1521eead79a124a65223eb4
EnGentech <engen.inyang@gmail.com>
2023-11-07 00:52:33 +0100

    401 error

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -14,6 +14,15 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
+def unauthorised() -> str:
+    """ GET /api/v1/anauthorized
+    Return:
+      - 401 error code
+    """
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
