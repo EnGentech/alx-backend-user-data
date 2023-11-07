0f0b3c75136387e0aaff7bf7214355491b29f252
EnGentech <engen.inyang@gmail.com>
2023-11-08 00:21:58 +0100

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class definition"""
-    pass
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """class method definition"""
+        if authorization_header is None or \
+            type(authorization_header) is not str \
+                or not authorization_header.startswith("Basic "):
+            return None
+        else:
+            basic, authenticator = authorization_header.split()
+            return authenticator
