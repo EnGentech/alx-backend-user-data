76d54fcdf351ba400e9449cc25119fb76d8c8359
EnGentech <engen.inyang@gmail.com>
2023-11-08 01:20:03 +0100

    completed test cases

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,14 @@ class BasicAuth(Auth):
             return decodeValue
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """credential extract"""
+        if decoded_base64_authorization_header is None or \
+                type(decoded_base64_authorization_header) is not str\
+                or ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        else:
+            key, value = decoded_base64_authorization_header.split(":")
+            return (key, value)
