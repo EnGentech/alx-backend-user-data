4c2ee0e2e1f8fbdf84ce61410e4d03b3094bda3d
EnGentech <engen.inyang@gmail.com>
2023-11-14 13:27:52 +0100

    created session from a function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,8 +32,7 @@ class DB:
 
     def add_user(self, email, hashed_password):
         """return User object and save to db"""
-        with self._session as session:
-            new_user = User(email=email, hashed_password=hashed_password)
-            session.add(new_user)
-            session.commit()
-            return new_user
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
