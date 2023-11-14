4adfcbf8846a2242bf4e312f9f3dfd6c0e95143b
EnGentech <engen.inyang@gmail.com>
2023-11-14 12:44:07 +0100

    adjusted size

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,7 @@ This code creates basic model for users table in sql
 """
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String(250)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -15,7 +15,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
