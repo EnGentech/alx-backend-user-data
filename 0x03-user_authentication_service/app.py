8462c77896aab9a743fc63702b8790da044ca64d
EnGentech <engen.inyang@gmail.com>
2023-11-15 18:40:57 +0100

    created a post route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,16 +3,30 @@
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
 def main():
     """basic route config"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def user():
+    """check for registered users"""
+    mail = request.form.get('email')
+    passcode = request.form.get('password')
+    try:
+        AUTH.register_user(email=mail, password=passcode)
+        return jsonify({"email": f"{mail}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
