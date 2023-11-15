7cace2c59dc43a54dc039b5d9402e4b4998d4a87
EnGentech <engen.inyang@gmail.com>
2023-11-15 09:54:06 +0100

    auth created with hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,8 @@
 
 
 import bcrypt
+from db import DB, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,22 @@ def _hash_password(password: str) -> bytes:
     password = password.encode("utf-8")
     hashed_passcode = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_passcode
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """class instance init"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """user registration for basic auth"""
+        try:
+            response = self._db.find_user_by(email=email)
+            if response:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashPasscode = _hash_password(password)
+            self._db.add_user(email, hashPasscode)
